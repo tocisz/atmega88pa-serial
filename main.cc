10762fe72865e3d6daad6c6f2fc178ff41bab57a
Tomasz Cichocki <cichymail@gmail.com>
2017-04-20 22:11:30 +0200

    try to decode sequence
    (but there are some bugs still...)

--- main.cc
+++ main.cc
@@ -30,33 +30,42 @@ static inline void print_pair(uint16_t a, uint16_t b);
 class Capture {
 public:
 	uint8_t capture_read_ptr = 0;
-
 	uint8_t state;
 
+private:
 	uint16_t s_short;
 	uint16_t s_long;
 	uint16_t s_cycle;
 
-	uint16_t avg_short;
-	uint16_t avg_long;
 	uint16_t avg_cycle;
+	uint16_t bit_threshold;
 
 	uint16_t cycle_1st_low;
 	uint16_t cycle_1st_high;
 
+	uint8_t current_bit_start;
+public: // XXX
+	int16_t bit_sequence_length;
+	uint16_t bit_sequence_high;
+
+public:
 	Capture() {
-		init_caputre();
+		init_capture();
 	}
 
-	void init_caputre();
 	void process_capture();
 
+private:
+	void init_capture();
+	void capture_init_sequence();
+	void capture_bit();
+	void start_new_bit();
 };
 
 
 #define BETWEEN(a,x,b) ((a<=x)&&(x<=b))
 
-void Capture::init_caputre() {
+void Capture::init_capture() {
 	state = 0;
 	s_short = 0;
 	s_long = 0;
@@ -65,14 +74,99 @@ void Capture::init_caputre() {
 
 void Capture::process_capture() {
 	while ((capture_write_ptr&~1) != (capture_read_ptr&~1)) {
-		uint8_t i_up = capture_read_ptr;
-		uint8_t i_down = (capture_read_ptr+1)%256;
-		uint16_t l_up = capture_buffer[i_up];
-		uint16_t l_down = capture_buffer[i_down];
-		uint16_t l_sum = l_up + l_down;
+		if (state == 7) {
+			capture_bit();
+		} else {
+			capture_init_sequence();
+		}
+	}
+}
+
+inline void read_pair(uint16_t &l_up, uint16_t &l_down, uint16_t &l_sum, uint8_t i) {
+	l_up = capture_buffer[i];
+	l_down = capture_buffer[(i+1)%256];
+	l_sum = l_up + l_down;
+}
+
+inline void read_pair(uint16_t &l_up, uint16_t &l_sum, uint8_t i) {
+	l_up = capture_buffer[i];
+	uint16_t l_down = capture_buffer[(i+1)%256];
+	l_sum = l_up + l_down;
+}
+
+void Capture::start_new_bit() {
+	current_bit_start = capture_read_ptr;
+	bit_sequence_length = 0;
+	bit_sequence_high = 0;
+}
+
+void emit_bit(uint8_t bit) {
+	putchar('0'+bit);
+	while (out_buf_length() > 0); //wait
+}
+
+void Capture::capture_bit() {
+	uint16_t l_up, l_sum;
+	while ((capture_write_ptr&~1) != (capture_read_ptr&~1)) {
+		read_pair(l_up, l_sum, capture_read_ptr);
+
+		bit_sequence_high += l_up;
+		bit_sequence_length += l_sum;
+
+		if (bit_sequence_length > avg_cycle) {
+			uint16_t deviation;
+			uint16_t deviation_plus = bit_sequence_length - avg_cycle;
+			// go back to find best match
+			uint8_t try_idx = (capture_read_ptr-2)%256;
+			read_pair(l_up, l_sum, try_idx);
+			uint16_t deviation_minus = avg_cycle - (bit_sequence_length-l_sum);
+			if (deviation_minus < deviation_plus) { // go back one pair
+				putchar('-');
+				capture_read_ptr = try_idx;
+				deviation = deviation_minus;
+				bit_sequence_high -= l_up;
+			} else {
+				putchar('+');
+				deviation = deviation_plus;
+			}
+
+			if (deviation > avg_cycle / 4) {
+				if (deviation < avg_cycle / 2) {
+					// try to correct timing
+					emit_bit((bit_sequence_high > bit_threshold) ? 3 : 2);
+					current_bit_start = capture_read_ptr;
+					if (deviation_minus < deviation_plus) { // cycle too short
+						bit_sequence_length = deviation;
+						bit_sequence_high = 0;
+					} else { // cycle too long
+						// yes, we start from negative value to correct cycle boundary
+						bit_sequence_length = -deviation;
+						// and assume that high state was delayed
+						bit_sequence_high = deviation;
+					}
+				} else {
+					putchar('\n');
+					init_capture();
+					break;
+				}
+			} else {
+				emit_bit((bit_sequence_high > bit_threshold) ? 1 : 0);
+				start_new_bit();
+			}
+
+		}
+
+		capture_read_ptr = (capture_read_ptr+2)%256;
+	}
+}
+
+void Capture::capture_init_sequence() {
+	while ((capture_write_ptr&~1) != (capture_read_ptr&~1)) {
+		uint16_t l_up, l_down, l_sum;
+		read_pair(l_up, l_down, l_sum, capture_read_ptr);
 		if (BETWEEN(2500, l_up, 5000) && BETWEEN(2500, l_down, 5000)) {
-			cycle_1st_high = l_sum / 2; // 1/2 *2/3 * 5/4 + 1/6;
-			cycle_1st_low = cycle_1st_high / 2; // 1/2 *2/3 * 3/4;
+			cycle_1st_high = l_sum / 2; // 1/2 * (2/3 * 5/4 + 1/6);
+			cycle_1st_low = cycle_1st_high / 2; // 1/2 * (2/3 * 3/4);
 			state = 1;
 		} else if ((state == 1 || state == 2 || state == 5)
 			&& (BETWEEN(250, l_up, 1000) && BETWEEN(cycle_1st_low, l_sum, cycle_1st_high))) {
@@ -88,19 +182,24 @@ void Capture::process_capture() {
 			putchar('F'); putchar('0'+state);
 			print_pair(l_up, l_down);
 			print_pair(cycle_1st_low, cycle_1st_high);
-			init_caputre();
+			init_capture(); // go back to state 0
+			break; // break to reprocess
+			// pair that caused error may be start of a new init sequence
 		}
 
 		capture_read_ptr = (capture_read_ptr+2)%256;
-	}
-	if (state == 6) {
-		avg_short = s_short / 3;
-		avg_long = s_long / 3;
-		avg_cycle = s_cycle / 6;
-		print_param("LO", avg_short);
-		print_param("HI", avg_long);
-		print_param("CY", avg_cycle);
-		init_caputre();
+
+		if (state == 7) {
+			uint16_t avg_short = s_short / 3;
+			uint16_t avg_long = s_long / 3;
+			bit_threshold = (avg_short + avg_long) / 2;
+			avg_cycle = s_cycle / 6;
+			print_param("LO ", avg_short);
+			print_param("HI ", avg_long);
+			print_param("CY ", avg_cycle);
+			start_new_bit();
+			break;
+		}
 	}
 }
 ///////////////////////////////////////////
@@ -183,6 +282,10 @@ int main(void)
 						print_param("WP ", capture_write_ptr);
 					} else if (c == 'r') {
 						print_param("RP ", capture.capture_read_ptr);
+					} else if (c == 'l') {
+						print_param("BL ", capture.bit_sequence_length);
+					} else if (c == 'h') {
+						print_param("BH ", capture.bit_sequence_high);
 					} else {
 						putchar(c);
 					}
