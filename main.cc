ed5af9f5205dc18a181bf9b4cef510375b30863f
Tomasz Cichocki <cichymail@gmail.com>
2017-06-12 23:02:18 +0200

    cellular automaton rule 30

--- main.cc
+++ main.cc
@@ -30,6 +30,67 @@ void print_time(uint16_t val) {
 
 #include "nokia_display.h"
 
+////////////////////////////////////////////
+
+class Automaton30 {
+	static const size_t line_size = 11;
+	uint8_t line[2][line_size];
+	uint8_t gen;
+
+public:
+	Automaton30() {
+		gen = 0;
+		memset(line[gen], 0, line_size);
+		line[gen][5] = 1;
+	}
+
+	void generate() {
+		uint8_t nextgen = !gen;
+		uint8_t im = line_size-1;
+		uint8_t i = 0;
+		uint8_t ip = 1;
+		while(i < line_size) {
+			uint8_t vm = (((line[gen][im]&1) << 7) | (line[gen][i]>>1));
+			uint8_t v = line[gen][i];
+			uint8_t vp = (line[gen][i]<<1) | (line[gen][ip]>>7);
+			line[nextgen][i] = vm ^ (v | vp);
+
+			im = i;
+			++i;
+			ip = i+1;
+			if (ip == line_size) {
+				ip = 0;
+			}
+		}
+
+		gen = nextgen;
+	}
+
+	uint8_t* get_generation() {
+		return line[gen];
+	}
+
+	void draw_generation(NokiaGraphDisplay &display) {
+		uint8_t bit = 0;
+		uint8_t *ptr = line[gen];
+		uint8_t val;
+		for (uint8_t i = 0; i < 84; ++i) {
+			if (bit == 0) {
+				val = *(ptr++);
+				bit = 8;
+			}
+			display.put_pixel((val & 0b10000000) ? 1 : 0);
+			val <<= 1;
+			--bit;
+		}
+	}
+};
+
+////////////////////////////////////////////
+
+const uint8_t draw_delay = 1;
+uint8_t draw_cnt = draw_delay;
+
 int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
@@ -38,11 +99,11 @@ int main(void)
 	set_sleep_mode(SLEEP_MODE_IDLE);
 	cpu_irq_enable();
 
-	NokiaTextDisplay display;
+	NokiaGraphDisplay display;
 
 	display.init(4, 60);
-	display.print("Ready\n");
-	display.set_cursor_delay(244);
+
+	Automaton30 automaton;
 
 	/* Replace with your application code */
 	for(;;) {
@@ -68,14 +129,19 @@ int main(void)
 				NONATOMIC_BLOCK(NONATOMIC_FORCEOFF) {
 					wdt_reset();
 					animate_glow();
-					display.animate_cursor();
+
+					--draw_cnt;
+					if (draw_cnt == 0) {
+						automaton.generate();
+						automaton.draw_generation(display);
+						draw_cnt = draw_delay;
+					}
 				}
 			}
 
 			while (in_buf_length() > 0) {
 				char c = getchar();
 				putchar(c);
-				display.print(c);
 			}
 		}
 
